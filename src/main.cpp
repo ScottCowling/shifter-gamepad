b1a59ee7255df7ec1fbc20633203aa865c8c5e60
Scott Cowling <scott.cowling14@gmail.com>
2022-04-04 01:54:13 +0100

    fixed logging and poll rate option WIP

--- src/main.cpp
+++ src/main.cpp
@@ -3,74 +3,108 @@
 #include <HID-Project.h>
 #include <HID-Settings.h>
 
-//#define DEBUG_MODE
-#define LED_ENABLE
+////Enable debug logging via Serial.
+#define DEBUG_MODE
+
+////Wait for a Serial connection before initializing.
+//#define WAIT_FOR_DEBUG
+
+////Enables RGB led indicator for button presses.
+//#define LED_ENABLE
 
 #ifdef LED_ENABLE
-#define LED_BRIGHTNESS 255
-#define LED_NUMPIXELS 1
+#define LED_BRIGHTNESS  255
+#define LED_NUMPIXELS   1
 #define LED_DATA_PIN    7
-#define LED_CLK_PIN   8
+#define LED_CLK_PIN     8
 int buttonColours[5] = { 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF };
 Adafruit_DotStar led(LED_NUMPIXELS, LED_DATA_PIN, LED_CLK_PIN, DOTSTAR_BRG);
 #endif
 
-/*
-  #define SYS_LED_PIN = LED_BUILTIN;
-  pinMode(SYS_LED_PIN, OUTPUT);
-  digitalWrite(SYS_LED_PIN, HIGH);
-  digitalWrite(SYS_LED_PIN, LOW);
-*/
+#define SYS_LED_PIN LED_BUILTIN
+
+#define POLL_TARGET_RATE 125
+
 
 int buttonCount = 5;
 int buttonPins[5] = { 0, 1, 2, 3, 4 } ;
 bool buttonLastStates[5] = { false, false, false, false, false };
 
+unsigned long pollStartTime = 0;
+unsigned long pollTargetTime = 0;
+unsigned long loops = 0;
+
 void setup() {
-#ifdef LED_ENABLE
-  led.begin();
-  led.setBrightness(LED_BRIGHTNESS);
-  led.show();
+  pinMode(SYS_LED_PIN, OUTPUT); 
+  digitalWrite(SYS_LED_PIN, HIGH); //Signal start of boot process on LED_BUILTIN
+
+#ifdef DEBUG_MODE
+  #pragma message "BUILDING WITH DEBUG ENABLED"
+  Serial.begin(115200);
+#ifdef WAIT_FOR_DEBUG
+  #pragma message "BUILD WILL WAIT FOR SERIAL"
+  while (!Serial) { }
+#endif
 #endif
 
   Gamepad.begin();
   for (int i = 0; i < buttonCount; i++) {
     pinMode(i, INPUT_PULLUP);
   }
-  
-#ifdef DEBUG_MODE
-  Serial.begin(9600);
-  while (!Serial) { }
+
+#ifdef LED_ENABLE
+ #pragma message "BUILD WITH LED DIAGNOSTIC"
+  led.begin();
+  led.setBrightness(LED_BRIGHTNESS);
+  led.show();
 #endif
+
+  pollTargetTime = 1000 / POLL_TARGET_RATE;
+  
+  digitalWrite(SYS_LED_PIN, LOW); //Signal end of boot process on LED_BUILTIN
 }
 
 void loop() {
-  led.clear();
+  unsigned long split = millis() - pollStartTime;
+  int delayV =  pollTargetTime - split;
+  if (delayV > 0) { delay(delayV); }
+  pollStartTime = millis();
+  loops++;
+
+  if (loops % POLL_TARGET_RATE == 0) {
+    Serial.print("LOOPS ");
+    Serial.println(loops);
+  }
+
   for (int i = 0; i < buttonCount; i++) {
     if (!digitalRead(i)) {
-#ifdef DEUBG_MODE
+#ifdef DEBUG_MODE
       if (!buttonLastStates[i]) {
         Serial.print("PRESSED ");
         Serial.println(i);
       }
-#endif
-      Gamepad.press(i + 1);
       buttonLastStates[i] = true;
+#endif
+
+      Gamepad.press(i + 1);
 #ifdef LED_ENABLE
       led.setPixelColor(0, buttonColours[i]);
+      led.show();
 #endif
     } else {
-#ifdef DEUBG_MODE
+#ifdef DEBUG_MODE
       if (buttonLastStates[i]) {
         Serial.print("RELEASED ");
         Serial.println(i);
       }
-     - 
+#endif
+#if LED_ENABLE
+      led.clear();
+      led.show();
 #endif
       Gamepad.release(i + 1);
       buttonLastStates[i] = false;
     }
   }
-  Gamepad.write();
-  led.show();
+  Gamepad.write(); //Write gamepad state.
 }
