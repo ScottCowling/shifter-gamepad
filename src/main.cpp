1409d1838592052fe10532a741e879889aa0302d
Scott Cowling <scott.cowling14@gmail.com>
2022-04-04 20:56:23 +0100

    LED_BUILTIN indicator light, pragma regions

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,70 @@
 #include <HID-Project.h>
 #include <HID-Settings.h>
 
-//Set the rate the device will report back to the USB host. 
-#define TARGET_REPORT_RATE 125
+#pragma region Firmware Options                       //Tweak firmware options before compiling such as enabling debug mode and changing report rate.
 
-//Enable debug logging via Serial.
-#define DEBUG_MODE
+#define TARGET_REPORT_RATE     125                    //Set the rate the device will report back to the USB host. Remove it to disable target report rate.
 
-//Wait for a Serial connection before initializing.
-#define WAIT_FOR_DEBUG
+#define DEBUG_MODE                                    //Enable debug logging via Serial.
+#define WAIT_FOR_DEBUG                                //Wait for a Serial connection before initializing.
 
-//Enables RGB led indicator for button presses.
-#define LED_ENABLE
+#define DOTSTAR_STATUS_LED_ENABLE                     //Enable RGB DotStar as an led indicator for button presses.
+#define DOTSTAR_STATUS_LED_BRIGHTNESS 255             //DotStar brightness (0-255).
+#define DOTSTAR_STATUS_LED_DATA       7               //DotStar data pin.
+#define DOTSTAR_STATUS_LED_CLK        8               //DotStar clock pin.
+#define DOTSTAR_STATUS_LED_COUNT      1               //DotStar single led on Trinket M0.
+#define DOTSTAR_STATUS_LED_FORMAT     DOTSTAR_BRG     //DotStar colour format e.g. RGB, BRG, BGR.
 
-#ifdef LED_ENABLE
-#define LED_BRIGHTNESS  255
-#define LED_NUMPIXELS   1
-#define LED_DATA_PIN    7
-#define LED_CLK_PIN     8
-int buttonColours[5] = { 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF };
-Adafruit_DotStar led(LED_NUMPIXELS, LED_DATA_PIN, LED_CLK_PIN, DOTSTAR_BRG);
+#pragma endregion
+
+#ifdef DEBUG_MODE
+#ifdef WAIT_FOR_DEBUG
+#pragma message "WARNING: BUILDING WITH DEBUG TOOLING ENABLED!"
+#endif
 #endif
 
-#define SYS_LED_PIN LED_BUILTIN
+#pragma region Debug
 
 
 
+#pragma endregion
+
+
+#pragma region DotStar LED Indicator
+
+#ifdef DOTSTAR_STATUS_LED_ENABLE
+int buttonColours[5] = { 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF };
+Adafruit_DotStar led(DOTSTAR_STATUS_LED_COUNT, DOTSTAR_STATUS_LED_DATA, DOTSTAR_STATUS_LED_CLK, DOTSTAR_STATUS_LED_FORMAT);
+#endif
+
+void dotstar_init() {
+#ifdef DOTSTAR_STATUS_LED_ENABLE
+  led.begin();
+  led.setBrightness(DOTSTAR_STATUS_LED_BRIGHTNESS);
+  led.show();
+#endif
+}
+
+#pragma endregion
+
+#pragma region Status LED Indicator
+//TODO: turn on led if any button pressed, off if not.
+void status_init() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+void status_on() {
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+void status_off() {
+  digitalWrite(LED_BUILTIN, LOW);
+}
+void status_wait_blink() {
+  status_off();
+  delay(250);
+  status_on();
+}
+#pragma endregion
 
 int buttonCount = 5;
 int buttonPins[5] = { 0, 1, 2, 3, 4 };
@@ -38,15 +77,12 @@ unsigned long targetReportTime = 0;
 unsigned long reportCounter = 0;
 
 void setup() {
-  pinMode(SYS_LED_PIN, OUTPUT); 
-  digitalWrite(SYS_LED_PIN, HIGH); //Signal start of boot process on LED_BUILTIN
+  status_init();
 
 #ifdef DEBUG_MODE
-  #pragma message "BUILDING WITH DEBUG ENABLED"
   Serial.begin(115200);
 #ifdef WAIT_FOR_DEBUG
-  #pragma message "BUILD WILL WAIT FOR SERIAL"
-  while (!Serial) { }
+  while (!Serial) { status_wait_blink(); }
 #endif
 #endif
 
@@ -55,16 +91,11 @@ void setup() {
     pinMode(i, INPUT_PULLUP);
   }
 
-#ifdef LED_ENABLE
- #pragma message "BUILD WITH LED DIAGNOSTIC"
-  led.begin();
-  led.setBrightness(LED_BRIGHTNESS);
-  led.show();
-#endif
+  dotstar_init();
 
   targetReportTime = 1000 / TARGET_REPORT_RATE;
   
-  digitalWrite(SYS_LED_PIN, LOW); //Signal end of boot process on LED_BUILTIN
+  digitalWrite(LED_BUILTIN, LOW); //Signal end of boot process on DOTSTAR_STATUS_LED_BUILTIN
 }
 
 void loop() {
@@ -76,7 +107,7 @@ void loop() {
     //Serial.print("REPORT LAG: "); 
     //Serial.print(reportDelay * -1); 
     //Serial.println("ms longer than allocated!"); 
-    Serial.print("SLOW REPORTING. ");
+    Serial.print("SLOW. ");
     Serial.print(1000 / reportTimeDifference);
     Serial.println("hz");
   }
@@ -99,7 +130,7 @@ void loop() {
 #endif
 
       Gamepad.press(i + 1);
-#ifdef LED_ENABLE
+#ifdef DOTSTAR_STATUS_LED_ENABLE
       led.setPixelColor(0, buttonColours[i]);
       led.show();
 #endif
@@ -110,7 +141,7 @@ void loop() {
         Serial.println(i);
       }
 #endif
-#ifdef LED_ENABLE
+#ifdef DOTSTAR_STATUS_LED_ENABLE
       led.clear();
       led.show();
 #endif
