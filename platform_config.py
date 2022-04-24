a0cb10f1b169f3c0bb05e6309fd7620d0cecb47b
Scott Cowling <scott.cowling14@gmail.com>
2022-04-24 17:21:34 +0100

    disabled platformio build script by default

--- platform_config.py
+++ platform_config.py
@@ -1,3 +1,9 @@
+#Remove the block comment below to allow the USB PID-VID & Product code to run to change the identity of your device. 
+#Note that the default values are pid.codes testing values and NOT recommended for production use. 
+#See pid.codes or the USB-IF for guidance on attaining a product id so you can give your device a production name.
+#You can also put any custom advanced build process code here where platformio.ini does not allow.
+
+"""
 Import("env")
 
 board_config = env.BoardConfig()
@@ -7,3 +13,4 @@ board_config.update("build.hwids", [
     ["0x1209", "0001"]
 ])
 board_config.update("build.usb_product", "JoyShift HGV")
+"""
